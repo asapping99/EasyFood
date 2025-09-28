1d46f84cb8a666df94a3c492152f76c545cc6e64
asapping99 <asapping99@gmail.com>
2025-09-29 00:29:04 +0900

    회원가인/로그인 시 충돌 처리

--- server/src/main/java/com/krecipe/KRecipeApplication.java
+++ server/src/main/java/com/krecipe/KRecipeApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class KRecipeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(KRecipeApplication.class, args);
         System.out.println("=====================================");
